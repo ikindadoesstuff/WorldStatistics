c1863fcf76e299a740200a76ba027965504ea668
Devyon Lozano <devyonlozano@gmail.com>
2025-11-06 14:50:35 -0500

    Separate concerns. Report methods go to ReportGenerator class

---
+++ src/main/java/com/napier/group21/ReportGenerator.java
@@ -0,0 +1,183 @@
+package com.napier.group21;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ReportGenerator {
+    public ArrayList<String> continents = new ArrayList<>();
+    public ArrayList<String> regions = new ArrayList<>();
+
+    public enum Scope {
+        WORLD,
+        CONTINENT,
+        REGION
+    }
+
+    public Connection conn = null;
+
+    // Connects to 'world' database on the MySQL server container
+    final String url = "jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false";
+    final String user = "root";
+    final String password = "group21";
+
+    public ReportGenerator() {
+        connect();
+        getContinents();
+        getRegions();
+    }
+
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Failed to load SQL driver: " + e.getMessage());
+            System.exit(1);
+        }
+
+
+        int retries = 30;
+        int retryWaitTime = 10000;
+        for (int i = 0; i < retries; i++) {
+            System.out.println("Connecting to database... (Attempt #" + (i + 1) + "/" + retries + ")");
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                System.out.println("Connected to database successfully");
+                break;
+
+            } catch (SQLException sqle) {
+
+                try {// Wait for database on fail
+                    Thread.sleep(retryWaitTime);
+                } catch (InterruptedException ie) {
+                    System.out.println(ie.getMessage());
+                }
+                System.out.println("Failed to connect to database: " + sqle.getMessage());
+            }
+        }
+    }
+
+    public void disconnect() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Failed to properly close database connection: " + e.getMessage());
+            }
+        }
+    }
+
+    public void generateSortedCountryReport() {
+        generateSortedCountryReport(Scope.WORLD, "");
+    }
+
+    public void generateSortedCountryReport(Scope scope, String name) {
+        name = name.toUpperCase();
+        String condition = "";
+        switch (scope) {
+            case WORLD:
+                System.out.println(
+                        "Displaying all countries in the world. " +
+                        "Population sorted, largest to smallest: "
+                );
+                break;
+            case CONTINENT:
+                System.out.printf(
+                        "Displaying all countries in continent - %s. " +
+                        "Population sorted, largest to smallest: \n"
+                        , name
+                );
+                if (!continents.contains(name)) {
+                    System.out.printf("Continent '%s' not found. Report can not be generated.\n", name);
+                    return;
+                } else {
+                    condition = String.format("WHERE Continent = '%s'", name);
+                    break;
+                }
+            case REGION:
+                System.out.printf(
+                        "Displaying all countries in region - %s. " +
+                        "Population sorted, largest to smallest: \n"
+                        , name
+                );
+                if (!regions.contains(name)) {
+                    System.out.printf("Region '%s' not found. Report can not be generated.\n", name);
+                    return;
+                } else {
+                    condition = String.format("WHERE Region = '%s'", name);
+                    break;
+                }
+        }
+
+        String query = String.format(
+                "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, capital.Name " +
+                "FROM country " +
+                "LEFT JOIN city as capital on country.Capital = capital.ID " +
+                "%s " +
+                "ORDER BY country.Population DESC",
+                condition
+        );
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            Thread.sleep(3000);
+            System.out.println("=====================================================================================");
+
+            while (resultSet.next()) {
+                String code = resultSet.getString("country.Code");
+                String cname = resultSet.getString("country.Name");
+                String continent = resultSet.getString("country.Continent");
+                String region = resultSet.getString("country.Region");
+                long population = resultSet.getLong("country.Population");
+                String capital = resultSet.getString("capital.name");
+                capital = capital != null ? capital : "None";
+
+                String result = String.format(
+//                        "%s (%s), %s, %s \nPopulation: %,d \nCapital: %s \n",
+                        "> %-45s %s | %-34s | Population: %,13d | Capital: %s ",
+                        cname, code, (continent + ", " + region), population, capital
+                );
+                System.out.println(result);
+            }
+            System.out.println("\n");
+        } catch (SQLException sqle) {
+            System.out.println("Failed to execute statement: " + sqle.getMessage());
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+    }
+
+    public void getContinents() {
+        String query = "SELECT DISTINCT Continent FROM country";
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String continent = resultSet.getString("Continent");
+                continents.add(continent.toUpperCase());
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Failed to execute statement: " + sqle.getMessage());
+        }
+    }
+
+    public void getRegions() {
+        String query = "SELECT DISTINCT Region FROM country";
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String region = resultSet.getString("Region");
+                regions.add(region.toUpperCase());
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Failed to execute statement: " + sqle.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
