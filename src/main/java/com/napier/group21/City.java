2b922630caa278e32b48658cf69da6b3fa5679ef
Devyon Lozano <devyonlozano@gmail.com>
2025-11-18 18:48:12 -0500

    Add City record

---
+++ src/main/java/com/napier/group21/City.java
@@ -0,0 +1,21 @@
+package com.napier.group21;
+
+/** 
+ * Using a record rather than a class is more semantically correct, as the records we get from the database should
+ * be final. It also removes the need to declare a constructor method.
+ * @param population Population is of type long because the maximum value of int is only +2,147,483,647
+ */
+public record City(String name, String country, String district, long population) implements DatabaseObject {
+    @Override
+    public String toString() {
+        return "> %-45s │ %-34s │ %-30s │ %,13d "
+                .formatted(name, country, district, population);
+    }
+
+    public String getColumnString() {
+        String columns = "\n%-47s │ %-34s │ %-30s │ %s  \n"
+                .formatted("City", "Country", "District", "Population");
+        String separator = "━".repeat(columns.length() + 10);
+        return separator + columns + separator;
+    }
+}
