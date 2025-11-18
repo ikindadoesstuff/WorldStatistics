29ddb2c1d8862eea2225e56f10f90f012a824e63
Devyon Lozano <devyonlozano@gmail.com>
2025-11-18 18:50:21 -0500

    Update Country record javadoc

--- src/main/java/com/napier/group21/Country.java
+++ src/main/java/com/napier/group21/Country.java
@@ -1,13 +1,9 @@
 package com.napier.group21;
 
-/** 
- * Using a record rather than a class simplifies the process of actually using Country objects.
- * This is because the data we get from the database should be immutable (shouldn't change).
- * Country, being a record, has getter and .toString() methods automatically, making the data
- * much easier to access. The code below is the simplest way to define a record.
- * |
- * The 'population' variables is of type 'long', because the maximum value of 'int' is only
- * +2,147,483,647
+/**
+ * Using a record rather than a class is more semantically correct, as the records we get from the database should
+ * be final. It also removes the need to declare a constructor method.
+ * @param population Population is of type long because the maximum value of int is only +2,147,483,647
  */
 public record Country (String code, String name, String continent, String region, long population, String capital) implements DatabaseObject {
     @Override
