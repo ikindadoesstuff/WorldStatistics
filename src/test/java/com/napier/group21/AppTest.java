a7fd4901aea9e19e6296c721064dcff531a0a06f
Devyon Lozano <devyonlozano@gmail.com>
2025-11-21 00:48:02 -0500

    Add unit test for printReport method in AppTest

--- src/test/java/com/napier/group21/AppTest.java
+++ src/test/java/com/napier/group21/AppTest.java
@@ -1,4 +1,22 @@
 package com.napier.group21;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static com.napier.group21.ReportGenerator.printReport;
+
 public class AppTest {
+    static App app;
+
+    @BeforeAll
+    static void init() {
+        app = new App();
+    }
+
+    @Test
+    void testPrintReportNull() {
+        printReport(null);
+    }
 }
