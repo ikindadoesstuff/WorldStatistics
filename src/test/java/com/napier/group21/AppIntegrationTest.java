fa8dc3bff5ac9690e1ea4dcdd289e9569761f0a9
Devyon Lozano <devyonlozano@gmail.com>
2025-11-23 15:47:20 -0500

    Add some city report tests

--- src/test/java/com/napier/group21/AppIntegrationTest.java
+++ src/test/java/com/napier/group21/AppIntegrationTest.java
@@ -44,9 +44,9 @@ public class AppIntegrationTest {
     // Sorted Country Reports
     @Test
     void testGenerateSortedCountryReport_GlobalScope() {
+        int expectedRows = 239;
         List<Country> countries;
         countries = reportGenerator.generateSortedCountryReport();
-        int expectedRows = 239;
 
         // Test Report Success
         assertNotNull(countries,
@@ -66,9 +66,9 @@ public class AppIntegrationTest {
 
     @Test
     void testGenerateSortedCountryReport_ContinentScope() {
+        int expectedRows = 58;
         List<Country> countries;
         countries = reportGenerator.generateSortedCountryReport(Scope.CONTINENT, "Africa");
-        int expectedRows = 58;
 
         // Test Report Success
         assertNotNull(countries,
@@ -88,9 +88,9 @@ public class AppIntegrationTest {
 
     @Test
     void testGenerateSortedCountryReport_RegionScope() {
+        int expectedRows = 24;
         List<Country> countries;
         countries = reportGenerator.generateSortedCountryReport(Scope.REGION, "Caribbean");
-        int expectedRows = 24;
 
         // Test Report Success
         assertNotNull(countries,
@@ -111,9 +111,9 @@ public class AppIntegrationTest {
     // Top N Country Reports
     @Test
     void testGenerateTopNCountryReport_GlobalScope() {
+        int expectedRows = 10;
         List<Country> countries;
-        countries = reportGenerator.generateSortedCountryReport();
-        int expectedRows = 239;
+        countries = reportGenerator.generateTopNCountryReport(expectedRows);
 
         // Test Report Success
         assertNotNull(countries, "Report ArrayList should not be null unless an error occurred");
@@ -125,9 +125,9 @@ public class AppIntegrationTest {
 
     @Test
     void testGenerateTopNCountryReport_ContinentScope() {
+        int expectedRows = 5;
         List<Country> countries;
-        countries = reportGenerator.generateSortedCountryReport(Scope.CONTINENT, "Africa");
-        int expectedRows = 58;
+        countries = reportGenerator.generateTopNCountryReport(Scope.CONTINENT, "North America", expectedRows);
 
         // Test Report Success
         assertNotNull(countries, "Report ArrayList should not be null unless an error occurred");
@@ -139,9 +139,9 @@ public class AppIntegrationTest {
 
     @Test
     void testGenerateTopNCountryReport_RegionScope() {
+        int expectedRows = 3;
         List<Country> countries;
-        countries = reportGenerator.generateSortedCountryReport(Scope.REGION, "Caribbean");
-        int expectedRows = 24;
+        countries = reportGenerator.generateTopNCountryReport(Scope.REGION, "Micronesia",  expectedRows);
 
         // Test Report Success
         assertNotNull(countries, "Report ArrayList should not be null unless an error occurred");
@@ -149,5 +149,109 @@ public class AppIntegrationTest {
         // Test Report Size Against Known Result
         assertEquals(expectedRows, countries.size(),
                 "There should be %d countries in the report".formatted(expectedRows));
+    }
+
+    // Sorted Country Reports
+    @Test
+    void testGenerateSortedCityReport_GlobalScope() {
+        int expectedRows = 4079;
+        List<City> cities;
+        cities = reportGenerator.generateSortedCityReport();
+
+        // Test Report Success
+        assertNotNull(cities,
+                "Report ArrayList should not be null unless an error occurred");
+
+        // Test Report Size Against Known Result
+        assertEquals(expectedRows, cities.size(),
+                "There should be %d cities in the report".formatted(expectedRows));
+
+        // Test Sorting
+        for (int i = 0; i < cities.size(); i++) {
+            if (i + 1 == cities.size()) break;
+            assertTrue(cities.get(i).population() >= cities.get(i+1).population(),
+                    "Report should be sorted in descending population order");
+        }
+    }
+
+    @Test
+    void testGenerateSortedCityReport_ContinentScope() {
+        int expectedRows = 841;
+        List<City> cities;
+        cities = reportGenerator.generateSortedCityReport(Scope.CONTINENT, "Europe");
+
+        // Test Report Success
+        assertNotNull(cities,
+                "Report ArrayList should not be null unless an error occurred");
+
+        // Test Report Size Against Known Result
+        assertEquals(expectedRows, cities.size(),
+                "There should be %d cities in the report".formatted(expectedRows));
+
+        // Test Sorting
+        for (int i = 0; i < cities.size(); i++) {
+            if (i + 1 == cities.size()) break;
+            assertTrue(cities.get(i).population() >= cities.get(i+1).population(),
+                    "Report should be sorted in descending population order");
+        }
+    }
+
+    @Test
+    void testGenerateSortedCityReport_RegionScope() {
+        int expectedRows = 156;
+        List<City> cities;
+        cities = reportGenerator.generateSortedCityReport(Scope.REGION, "Southern Europe");
+
+        // Test Report Success
+        assertNotNull(cities,
+                "Report ArrayList should not be null unless an error occurred");
+
+        // Test Report Size Against Known Result
+        assertEquals(expectedRows, cities.size(),
+                "There should be %d cities in the report".formatted(expectedRows));
+
+        // Test Sorting
+        for (int i = 0; i < cities.size(); i++) {
+            if (i + 1 == cities.size()) break;
+            assertTrue(cities.get(i).population() >= cities.get(i+1).population(),
+                    "Report should be sorted in descending population order");
+        }
+    }
+
+    @Test
+    void testGenerateSortedCityReport_CountryScope() {
+        int expectedRows = 1;
+        List<City> cities;
+        cities = reportGenerator.generateSortedCityReport(Scope.COUNTRY, "Saint Lucia");
+
+        // Test Report Success
+        assertNotNull(cities,
+                "Report ArrayList should not be null unless an error occurred");
+
+        // Test Report Size Against Known Result
+        assertEquals(expectedRows, cities.size(),
+                "There should be %d cities in the report".formatted(expectedRows));
+
+        // Test Sorting
+        for (int i = 0; i < cities.size(); i++) {
+            if (i + 1 == cities.size()) break;
+            assertTrue(cities.get(i).population() >= cities.get(i+1).population(),
+                    "Report should be sorted in descending population order");
+        }
+    }
+
+    // Top N Country Reports
+    @Test
+    void testGenerateTopNCityReport_GlobalScope() {
+        int expectedRows = 10;
+        List<City> cities;
+        cities = reportGenerator.generateTopNCityReport(expectedRows);
+
+        // Test Report Success
+        assertNotNull(cities, "Report ArrayList should not be null unless an error occurred");
+
+        // Test Report Size Against Known Result
+        assertEquals(expectedRows, cities.size(),
+                "There should be %d cities in the report".formatted(expectedRows));
     }
 }
