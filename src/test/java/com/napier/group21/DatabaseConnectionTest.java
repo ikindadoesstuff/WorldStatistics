3a4f31196eba068e20a8363e76ef6dc70023e83b
Devyon Lozano <devyonlozano@gmail.com>
2025-11-22 22:44:48 -0500

    Add DatabaseConnection unit tests

---
+++ src/test/java/com/napier/group21/DatabaseConnectionTest.java
@@ -0,0 +1,49 @@
+package com.napier.group21;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Test class for the DatabaseConnection class.
+ */
+public class DatabaseConnectionTest {
+    DatabaseConnection databaseConnection;
+
+    @Test
+    void testDatabaseConnection_NoArgs() {
+        databaseConnection = new DatabaseConnection();
+    }
+
+    @Test
+    void testDatabaseConnection_Args() {
+        String[] args = new String[]{"localhost:33060"};
+        databaseConnection = new DatabaseConnection(args);
+    }
+
+    @Test
+    void testDatabaseConnection_NullArgs() {
+        databaseConnection = new DatabaseConnection(null);
+    }
+
+    @Test
+    void testDatabaseConnection_NullArgsElement() {
+        String[] args = {null};
+        databaseConnection = new DatabaseConnection();
+    }
+
+    @Test
+    void testConnect() {
+        databaseConnection.connect(1);
+    }
+
+    @Test
+    void testConnect_InvalidRetries() {
+        databaseConnection.connect(-5);
+    }
+}
